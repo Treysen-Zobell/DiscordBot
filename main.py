d2d3782342f51491cdecae53ee28d613bf4f30f4
Treysen-Zobell <36913811+Treysen-Zobell@users.noreply.github.com>
2022-03-29 02:11:11 -0600

    Began merging game controls and discord inputs

--- main.py
+++ main.py
@@ -7,26 +7,29 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+from time import sleep
+from random import sample
+
 
 class ScoreDocument:
     def __init__(self):
         self.SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
         self.SPREADSHEET_ID = '11a_dWjSM0ikum4ZpmQ-zLVd_OoIargE8a81qx2q-IcU'
 
-        self.creds = None
+        self.credentials = None
         if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
-        if not self.creds or not creds.valid:
-            if self.creds and self.creds.expired and self.creds.refresh_token:
-                self.creds.refresh(Request())
+            self.credentials = Credentials.from_authorized_user_file('token.json', self.SCOPES)
+        if not self.credentials or not self.credentials.valid:
+            if self.credentials and self.credentials.expired and self.credentials.refresh_token:
+                self.credentials.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file('credentials.json', self.SCOPES)
-                self.creds = flow.run_local_server(port=0)
+                self.credentials = flow.run_local_server(port=0)
             with open('token.json', 'w') as token:
-                token.write(self.creds.to_json())
+                token.write(self.credentials.to_json())
 
         try:
-            self.service = build('sheets', 'v4', credentials=self.creds)
+            self.service = build('sheets', 'v4', credentials=self.credentials)
             self.sheet = self.service.spreadsheets()
 
         except HttpError as err:
@@ -78,30 +81,33 @@ class Player:
 
 
 class Team:
-    def __init__(self, team_name):
-        self.team_name = team_name
-        self.exempt_players = []
+    def __init__(self, name, imposter_count):
+        self.name = name
         self.players = []
+        self.imposter_count = imposter_count
 
-    def set_players(self, players):
-        self.players = [player for player in players if player not in self.exempt_players]
-
-    def exempt_players(self, players):
-        self.exempt_players.extend(players)
-
-    def unexempt_players(self, players):
-        self.exempt_players = [player for player in self.players if player not in players]
+    def set_players(self, players, exempt_players):
+        self.players = [player for player in players if player not in exempt_players]
 
     def score(self, winning_team_name, suspected_imposters):
-        winning_team = self.team_name == winning_team_name
+        winning_team = self.name == winning_team_name
         for player in self.players:
             player.score(winning_team, suspected_imposters)
+
+    def assign_imposters(self):
+        for player in self.players:
+            player.imposter = False
+
+        imposter_players = sample(self.players, self.imposter_count)
+        for imposter_player in imposter_players:
+            imposter_player.imposter = True
 
 
 class Game:
     score_document = None
     player_info = None
-    players = []
+    exempt_player_list = []
+    player_list = []
     teams = []
 
     @staticmethod
@@ -110,24 +116,47 @@ class Game:
         Game.player_info = Game.score_document.read_range('A2:E30')
 
         for name, score, games_played, imposter_wins, teammate_wins in Game.player_info:
-            Game.players.append(Player(name, score, games_played, imposter_wins, teammate_wins))
+            Game.player_list.append(Player(name, score, games_played, imposter_wins, teammate_wins))
 
     @staticmethod
-    def create_team(team_name):
-        Game.teams.append(Team(team_name))
+    def exempt_players(players):
+        Game.exempt_player_list.extend(players)
+
+    @staticmethod
+    def unexempt_players(players):
+        for player in players:
+            Game.exempt_player_list.remove(player)
+
+    @staticmethod
+    def create_team(team_name, imposter_count):
+        Game.teams.append(Team(team_name, imposter_count))
+
+    @staticmethod
+    def get_player(player_name):
+        if not any(player.name == player_name for player in Game.player_list):
+            Game.player_list.append(Player(player_name, 0, 0, 0, 0))
+        return [player for player in Game.player_list if player.name == player_name][0]
+
+    @staticmethod
+    def get_players(player_names, include_exempt=False):
+        players = [Game.get_player(player_name) for player_name in player_names]
+        if not include_exempt:
+            players = [player for player in players if player not in Game.exempt_player_list]
+        return players
 
     @staticmethod
     def update_players(team_name, player_names):
-        for player_name in player_names:
-            if not any(player.name == player_name for player in Game.players):
-                Game.players.append(Player(player_name, 0, 0, 0, 0))
-
         if not any(team.name == team_name for team in Game.teams):
             Game.teams.append(Team(team_name))
 
         team = [team for team in Game.teams if team.name == team_name][0]
-        players = [player for player in Game.players if player.name in player_names]
-        team.set_players(players)
+        players = Game.get_players(player_names)
+        team.set_players(players, Game.exempt_player_list)
+
+    @staticmethod
+    def assign_imposters():
+        for team in Game.teams:
+            team.assign_imposters()
 
 
 class Client(discord.Client):
@@ -145,12 +174,72 @@ class Client(discord.Client):
 
     async def on_message(self, message):
         """run on message in server or to bot"""
-        channel = message.channel
-        content = str(message.content)
+        if message.content.startswith('!changeling'):
+            print(f'{message.author} said in {message.channel} | {message.content}')
+
+            command = message.content.split()[1:]
+            if len(command) == 0:
+                await message.channel.send('Command has no args, use `!changeling -h` for help.')
+
+            if command[0] == 'create_team':
+                team_name = command[1]
+
+                imposter_count = 0
+                if len(command) == 3:
+                    imposter_count = int(command[2])
+
+                Game.create_team(team_name, imposter_count)
+                discord_users = await self.get_users_in_voice_channel(await self.get_channel_by_name(team_name))
+                player_names = [discord_user.name for discord_user in discord_users]
+                Game.update_players(team_name, player_names)
+
+                for team in Game.teams:
+                    for player in team.players:
+                        print('------------------------------------')
+                        print(f'{player.name} | {player.score} | {player.imposter_wins} | {player.teammate_wins} | {player.games_played}')
+
+            elif command[0] == 'update_team':
+                team_name = command[1]
+                discord_users = await self.get_users_in_voice_channel(await self.get_channel_by_name(team_name))
+                player_names = [discord_user.name for discord_user in discord_users]
+                Game.update_players(team_name, player_names)
+
+                for team in Game.teams:
+                    for player in team.players:
+                        print('------------------------------------')
+                        print(f'{player.name} | {player.score} | {player.imposter_wins} | {player.teammate_wins} | {player.games_played}')
+
+            elif command[0] == 'exempt_player':
+                names = command[1:]
+                players = Game.get_players(names)
+                Game.exempt_players(players)
+                print(Game.exempt_player_list)
+
+            elif command[0] == 'include_player':
+                names = command[1:]
+                players = Game.get_players(names, include_exempt=True)
+                Game.unexempt_players(players)
+                print(Game.exempt_player_list)
+
+            elif command[0] == 'start':
+                report_delay = int(command[1].split(':')[1]) * 60 + int(command[1].split(':')[0])
+                sleep(report_delay)
+                Game.assign_imposters()
+
+                for team in Game.teams:
+                    print(f'Team {team.name} players:')
+                    for player in team.players:
+                        print(f'    {player.name} is {"not" if not player.imposter else ""}the imposter')
+
+                # todo: stopping and voting things
 
 
 def main():
     Game.create()
+    for player in Game.player_list:
+        print(f'{player.name} | {player.score} | {player.imposter_wins} | {player.teammate_wins} | {player.games_played}')
+    client = Client()
+    client.run('ODMyNDI1MzIzNDg0MjgyOTMw.YHjmfg.HyA6wir8yQnU4WVG2Q9WzanyYHc')
 
 
 if __name__ == '__main__':
